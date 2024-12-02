8ebe6603df529b4bf602e14d0acf694809de1465
Miguel Delgado <delgdevoleper@gmail.com>
2024-12-02 18:57:26 -0300

    feature - docker-compose escrito, banco conectado... iniciar migrations

---
+++ src/main/java/challenge/Goomer/GoomerApplication.java
@@ -0,0 +1,13 @@
+package challenge.Goomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/Goomer/GoomerApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.Goomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
