11bb5853c8bbfeef12b5a9e08741f64da501bf5c
Miguel Delgado <delgdevoleper@gmail.com>
2024-12-05 23:16:57 -0300

    testing - testando a criacao de entidade restaurante

--- src/main/java/challenge/Goomer/infra/controller/RestController.java
+++ src/main/java/challenge/Goomer/infra/controller/RestController.java
@@ -1,4 +1,32 @@
 package challenge.Goomer.infra.controller;
 
+import challenge.Goomer.infra.entity.RestaurantEntity;
+import challenge.Goomer.infra.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @GetMapping("/criar")
+    public ResponseEntity<String> creating() {
+        var entidade = new RestaurantEntity();
+        entidade.setName("Example Restaurant");
+        entidade.setPhoto("example.jpg");
+        restaurantRepository.save(entidade);
+        var id = entidade.getId();
+        var created = entidade.getCreatedAt();
+        var updated = entidade.getUpdatedAt();
+        String message = String.format("Restaurant created with ID: %s, criado as: %s, atualizado as: %s", id, created, updated);
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/encontrar")
+    public ResponseEntity<Object> encontrar() {
+        return ResponseEntity.ok(restaurantRepository.findAll());
+    }
 }
